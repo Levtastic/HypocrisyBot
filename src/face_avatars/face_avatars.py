9ec7c6fd7ba2eea42bbdcdfdc6b51a95087527ff
Michael Fruen <mfruen@gmail.com>
2019-02-23 00:25:28 +0000

    Added timeouts to avatar change attempts, and new avatars are randomly chosen roughly once a day

--- src/face_avatars/face_avatars.py
+++ src/face_avatars/face_avatars.py
@@ -1,4 +1,6 @@
+import asyncio
 import aiohttp
+import random
 import logging
 
 from levbot import UserLevel, TypingContext
@@ -10,8 +12,6 @@ class FaceAvatars:
         self.bot = bot
         self.url = 'https://thispersondoesnotexist.com/image'
 
-        bot.register_event('on_ready', self.on_ready)
-
         bot.commands.register_handler(
             'refresh avatar',
             self.cmd_refresh_avatar,
@@ -21,30 +21,58 @@ class FaceAvatars:
             )
         )
 
-    async def on_ready(self):
-        if not await self.get_new_avatar():
-            logging.info('Startup face change failed')
+        self.bot.loop.create_task(self.avatar_loop())
+
+    async def avatar_loop(self):
+        await self.bot.wait_until_ready()
+
+        while not self.bot.is_closed:
+            if random.randint(1, 60 * 24) == 1:
+                await self.get_new_avatar()
+
+            await asyncio.sleep(60)
 
     async def get_new_avatar(self):
+        try:
+            bavatar = await asyncio.wait_for(self.get_face_image(), 10)
+
+        except asyncio.TimeoutError:
+            logging.info('Timed out fetching new face')
+            return False
+
+        if not bavatar:
+            return False
+
+        try:
+            if await asyncio.wait_for(self.set_avatar(bavatar), 10):
+                logging.info('New face avatar set.')
+                return True
+
+        except asyncio.TimeoutError:
+            logging.info('Timed out setting new face')
+
+        return False
+
+    async def get_face_image(self):
         async with aiohttp.ClientSession() as session:
             async with session.get(self.url) as resp:
                 if resp.status != 200:
+                    logging.warning(
+                        f'Received status {resp.status} from {self.url}')
                     return False
 
-                bavatar = await resp.read()
+                return await resp.read()
 
+    async def set_avatar(self, avatar):
         try:
-            await self.bot.edit_profile(
-                avatar=bavatar
-            )
+            await self.bot.edit_profile(avatar=avatar)
+            return True
 
-        except (HTTPException, InvalidArgument):
-            logging.exception(bavatar)
+        except (HTTPException, InvalidArgument) as ex:
+            savatar = str(avatar)
+            savatar if len(savatar) <= 1000 else savatar[:1000] + '...'
+            logging.exception(savatar)
             return False
-
-        logging.info('New face avatar set.')
-
-        return True
 
     async def cmd_refresh_avatar(self, message):
         with TypingContext(message.channel):
