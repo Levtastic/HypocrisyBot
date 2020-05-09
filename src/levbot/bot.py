Levtastic/HypocrisyBot

commit 995d67bfdc99314ac4280ba4b54a1c05a04a6ab6

Added --update-tables command line argument
This argument forces all models to delete and recreate their tables, ensuring data types are updated.
(Needed for 10404597637eb7b0e2ef983bdaf2db9d746ba306 changing TEXT fields to INT)

Also changed logging to be handled on bot init, instead of bot run, since models are updated without running the bot.

src/levbot/bot.py
@@ -18,7 +18,8 @@ class Bot(Client):
         self.settings = settings
 
         self.bot_token = settings.pop('bot_token')
-        self.pushbullet_token = settings.get('pushbullet_token', None)
+
+        self.logger = set_up_logging(settings.get('pushbullet_token', None))
 
         self._event_handlers = defaultdict(list)
         self.commands = Commands(self)
@@ -32,10 +33,12 @@ class Bot(Client):
 
         send_splitter.wrap(Messageable)
 
-    def run(self, *args, **kwargs):
-        set_up_logging(self.pushbullet_token)
-        super().run(self.bot_token, *args, **kwargs)
+    def close(self):
         remove_pushbullet_logger()
+        super().close()
+
+    def run(self, *args, **kwargs):
+        super().run(self.bot_token, *args, **kwargs)
 
     def event(self, event_name=''):
         def decorator_event(coro):

src/levbot/database/database.py
@@ -1,4 +1,5 @@
 import sqlite3
+import logging
 
 from contextlib import closing
 from .models import CommandAlias, User, UserGuild
@@ -27,6 +28,16 @@ class Database:
             type(self).__name__,
             name
         ))
+
+    def force_update_model_tables(self):
+        logging.info('Updating table layouts.')
+
+        for model_name in self.models.keys():
+            logging.info(f' - {model_name}...')
+            model = self.get_initialised_model(model_name)
+            model._update_table()
+
+        logging.info('Tables updated.')
 
     def add_models(self, *model_classes):
         for cls in model_classes:

src/main.py
@@ -1,3 +1,5 @@
+import sys
+import logging
 import settings
 
 from levbot import Bot
@@ -5,20 +7,34 @@ from vreddit.vreddit import VReddit
 from face_avatars.face_avatars import FaceAvatars
 
 
-settings = {v: getattr(settings, v) for v in dir(settings) if v[0] != '_'}
+def get_bot():
+    dsettings = {v: getattr(settings, v) for v in dir(settings) if v[0] != '_'}
 
-bot = Bot(settings)
+    bot = Bot(dsettings)
+
+    @bot.event()
+    async def on_ready():
+        print(f'Connected as {bot.user}')
+
+    VReddit(bot, dsettings['temp_directory'])
+    FaceAvatars(bot, dsettings['avatar_generator_url'])
+
+    return bot
 
 
-@bot.event()
-async def on_ready():
-    print(f'Connected as {bot.user}')
+if __name__ == '__main__':
+    bot = get_bot()
 
+    if '--update-tables' in sys.argv:
+        for handler in bot.logger.handlers:
+            if isinstance(handler, logging.StreamHandler):
+                handler.setLevel(logging.INFO)
 
-VReddit(bot, settings['temp_directory'])
-FaceAvatars(bot, settings['avatar_generator_url'])
+        bot.database.force_update_model_tables()
 
-bot.run()
+    else:
+        bot.run()
+
 
 # https://discordapi.com/permissions.html#499248208
 # https://github.com/daboth/pagan/blob/master/README.md

commit d112e0536293cb7471046ef8aa47966f4a8820b7

Fixed bot.close() error introduced in 995d67bfdc99314ac4280ba4b54a1c05a04a6ab6
bot.close() is meant to be a coroutine

src/levbot/bot.py
@@ -33,9 +33,9 @@ class Bot(Client):
 
         send_splitter.wrap(Messageable)
 
-    def close(self):
+    async def close(self):
         remove_pushbullet_logger()
-        super().close()
+        await super().close()
 
     def run(self, *args, **kwargs):
         super().run(self.bot_token, *args, **kwargs)